547446337651b306ebaa51176ea7ab4dbbab8a4e
Buster753 <>
2023-09-26 16:06:26 +0000

    javascript and html added

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let playerScore = document.getElementByClass(".playerScore")[0];
+let computerScore = 0;
+let choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+// document. ....
+
+
+function randomComputer(){
+    let randomChoice = Math.floor(math.random() * choices.length);
+    return choices[randomChoice]
+}
+
+function result(){
+    
+}
+
+function playGame(){
+    
+}
