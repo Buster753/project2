6d80036dc352f39191abdc74304541265726ea93
Buster753 <>
2023-09-27 04:16:49 +0000

    javascript and html and css added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,65 @@
-let playerScore = document.getElementByClass(".playerScore")[0];
+const playerScoreBoard = document.getElementById("playerScore");
+const computerScoreBoard = document.getElementById("computerScore");
+
+let choices = ["rock", "paper", "scissor", "lizard", "spock"];
+
+let playerScore = 0;
 let computerScore = 0;
-let choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-// document. ....
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissor = document.getElementById("scissor")
+const lizard = document.getElementById("lizard")
+const spock = document.getElementById("spock")
 
+const gameTitle = document.getElementById("gameTitle");
 
-function randomComputer(){
-    let randomChoice = Math.floor(math.random() * choices.length);
-    return choices[randomChoice]
+rock.addEventListener("click", function() {
+    result("rock")
+});
+scissor.addEventListener("click", function () {
+    result("paper");
+});
+paper.addEventListener("click", function () {
+    result("scissor");
+});
+lizard.addEventListener("click", function () {
+    result("lizard");
+});
+spock.addEventListener("click", function () {
+    result("spock");
+});
+
+function randomComputer() {
+    let randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
 }
 
-function result(){
-    
-}
-
-function playGame(){
-    
+function result(pChoice) {
+    let cChoice = randomComputer();
+    if (pChoice === cChoice) {
+        gameTitle.innerHTML = pChoice + " Does not beat " + cChoice + "<br>" + "     It is a Tie!"
+    }
+    else if (
+        (pChoice === "rock" && cChoice === "scissor") ||
+        (pChoice === "rock" && cChoice === "lizard") ||
+        (pChoice === "paper" && cChoice === "rock") ||
+        (pChoice === "paper" && cChoice === "spock") ||
+        (pChoice === "scissor" && cChoice === "paper") ||
+        (pChoice === "scissor" && cChoice === "lizard") ||
+        (pChoice === "lizard" && cChoice === "spock") ||
+        (pChoice === "lizard" && cChoice === "paper") ||
+        (pChoice === "spock" && cChoice === "scissor") ||
+        (pChoice === "spock" && cChoice === "rock")
+    ) {
+        gameTitle.innerHTML = pChoice + " beats " + cChoice + "<br>" + "      You Win!"
+        playerScore++;
+        playerScoreBoard.innerHTML = "Player: " + playerScore;
+        
+    } else {
+        gameTitle.innerHTML = pChoice + " gets beaten by " + cChoice + "<br>" + "      You Lose!"
+        computerScore++;
+        computerScoreBoard.innerHTML = "Computer: " + computerScore;
+        
+    }
 }
